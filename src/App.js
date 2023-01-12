f3e8440bb297f8023179af4a611ecac0426ddac9
Yogesh Rajput <Yogesh.Rajput@meritarc.com>
2023-01-12 21:15:42 +0530

    comment updated

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@
 * Product Page: https://www.creative-tim.com/product/material-dashboard-react
 * Copyright 2022 Creative Tim (https://www.creative-tim.com)
 
-Coded by www.creative-tim.com
-
  =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
